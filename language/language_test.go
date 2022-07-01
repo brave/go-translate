7f705af0b23841ff52711faa58aca15faeb71150
Moritz Haller <mhaller@brave.com>
2022-07-01 13:43:19 +0200

    wip

--- language/language_test.go
+++ language/language_test.go
@@ -1,14 +1,26 @@
 package language
 
 import (
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
-func TestToGoogleLanguageList(t *testing.T) {
-	msList := []byte(`{"translation":{"af":{"name":"Afrikaans","nativeName":"Afrikaans","dir":"ltr"},"en":{"name":"English","nativeName":"English","dir":"ltr"}}}`)
-	googleList := []byte(`{"sl":{"af":"Afrikaans","en":"English"},"tl":{"af":"Afrikaans","en":"English"}}`)
-	list, err := ToGoogleLanguageList(msList)
-	assert.Equal(t, nil, err)
-	assert.Equal(t, googleList, list)
+func TestToSourceResponseBody(t *testing.T) {
+	targetResBody := []byte(`{"translation":{"af":{"name":"Afrikaans","nativeName":"Afrikaans","dir":"ltr"},"en":{"name":"English","nativeName":"English","dir":"ltr"}}}`)
+	convertedResBody, err := ToSourceResponseBody(targetResBody)
+	expectedResBody := []byte(`{"sl":{"af":"Afrikaans","en":"English"},"tl":{"af":"Afrikaans","en":"English"}}`)
+
+	assert.Nil(t, err)
+	assert.Equal(t, expectedResBody, convertedResBody)
+}
+
+func TestIsSupportedLanguage(t *testing.T) {
+	assert.Equal(t, true, IsSupportedLanguage("de"))
+	assert.Equal(t, false, IsSupportedLanguage("ab"))
+}
+
+func TestShouldAutoDetectSourceLanguage(t *testing.T) {
+	assert.Equal(t, true, ShouldAutoDetectSourceLanguage("auto"))
+	assert.Equal(t, false, ShouldAutoDetectSourceLanguage("en"))
 }
--- translate/translate.go
+++ translate/translate.go
@@ -1,126 +1,80 @@
 package translate
 
 import (
-	"bytes"
 	"encoding/json"
 	"errors"
 	"net/http"
-	"net/url"
-	"strconv"
 
 	"github.com/brave/go-translate/language"
 )
 
-// RequestBody represents JSON format of Lingvanex requests.
-type RequestBody struct {
-	From          string   `json:"source"`
+type TranslationServiceRequestBody struct {
+	From          string   `json:"source,omitempty"`
 	To            string   `json:"target"`
 	Data          []string `json:"q"`
 	TranslateMode string   `json:"translateMode"`
 }
 
-// LingvanexResponseBody represents JSON format of Lingvanex response bodies.
-// Translations's size is limited to 1 since multiple translations is not
-// compatible with Google.
-// Format with auto-detect source language:
-//	[
-//		{
-//			"detectedLanguage": {"language": "de", "score": 1.0},
-//			"translations": [{"text": "Hallo", "to": "en"}]
-//		},
-//		{
-//			"detectedLanguage": {"language": "de", "score": 1.0},
-//			"translations": [{"text": "Welt", "to": "en"}]
-//		}
-//	]
-// Format without auto-detect source language:
-//	[
-//		{
-//			"translations": [{"text": "Hallo", "to": "en"}]
-//		},
-//		{
-//			"translations": [{"text": "Welt", "to": "en"}]
-//		}
-//	]
-//
-// score and to are not saved in this struct because we don't need them to
-// convert to a google format response.
-type LingvanexResponseBody struct {
+type TranslationServiceResponseBody struct {
 	SourceText     []string `json:"sourceText"`
 	TranslatedText []string `json:"translatedText"`
 }
 
-// ToLingvanexRequest parses the input Google format translate request and
-// return a corresponding Lingvanex format request.
-func ToLingvanexRequest(r *http.Request, serverURL string) (*http.Request, bool, error) {
-	lnxURL := serverURL
-	// Parse google format query parameters
-	slVals := r.URL.Query()["sl"]
-	if len(slVals) != 1 {
-		return nil, false, errors.New("invalid query parameter format: There should be one sl parameter")
+func BuildTargetRequestBody(reqFromSource *http.Request) ([]byte, error) {
+	// Parse source language code
+	slParameterValues := reqFromSource.URL.Query()["sl"]
+	if len(slParameterValues) != 1 {
+		return nil, errors.New("There should be exactly one sl parameter value")
 	}
-	tlVals := r.URL.Query()["tl"]
-	if len(tlVals) != 1 {
-		return nil, false, errors.New("invalid query parameter format: There should be one tl parameter")
-	}
-	from := slVals[0]
-	to := tlVals[0]
+	sourceLanguageCode := slParameterValues[0]
 
-	// Set Lnx format query parameters
-	u, err := url.Parse(lnxURL)
+	if !language.IsSupportedLanguage(sourceLanguageCode) && !language.ShouldAutoDetectSourceLanguage(sourceLanguageCode) {
+		return nil, errors.New("Source language code not supported")
+	}
+
+	// Parse target language code
+	tlParameterValues := reqFromSource.URL.Query()["tl"]
+	if len(tlParameterValues) != 1 {
+		return nil, errors.New("There should be exactly one tl parameter")
+	}
+	targetLanguageCode := tlParameterValues[0]
+
+	if !language.IsSupportedLanguage(targetLanguageCode) {
+		return nil, errors.New("Target language code not supported")
+	}
+
+	// Parse request body
+	err := reqFromSource.ParseForm()
 	if err != nil {
-		return nil, false, err
+		return nil, err
 	}
+	qParameterValues := reqFromSource.PostForm["q"]
 
-	// Convert Google format request body into Lnx format request body
-	err = r.ParseForm()
-	if err != nil {
-		return nil, false, err
+	var requestBody TranslationServiceRequestBody
+
+	// Only set `source` if source language is specified. Lingvanex attempts
+	// auto source language detection if `source` is missing
+	if !language.ShouldAutoDetectSourceLanguage(sourceLanguageCode) {
+		requestBody.From = sourceLanguageCode
 	}
-	qVals := r.PostForm["q"]
+	requestBody.To = targetLanguageCode
+	requestBody.TranslateMode = "html"
+	requestBody.Data = qParameterValues
 
-	lnx_from, err := language.ToLnxLanguageCode(from)
-	if err != nil {
-		return nil, false, errors.New("No matching lnx_from language code")
-	}
-
-	lnx_to, err := language.ToLnxLanguageCode(to)
-	if err != nil {
-		return nil, false, errors.New("No matching lnx_to language code")
-	}
-
-	var reqBody RequestBody
-	reqBody.From = lnx_from
-	reqBody.To = lnx_to
-	reqBody.TranslateMode = "html"
-	reqBody.Data = qVals
-
-	body, err := json.Marshal(reqBody)
-	if err != nil {
-		return nil, false, err
-	}
-
-	// Create the HTTP request
-	req, err := http.NewRequest("POST", u.String(), bytes.NewBuffer(body))
-	if err != nil {
-		return nil, false, err
-	}
-
-	// Set request headers
-	req.Header.Add("Content-Type", "application/json")
-	req.Header.Add("Content-Length", strconv.FormatInt(req.ContentLength, 10))
-	return req, from == "auto", nil
-}
-
-// ToGoogleResponseBody parses the input Lingvanex response and return the JSON
-// response body in Google format.
-func ToGoogleResponseBody(body []byte, isAuto bool) ([]byte, error) {
-	// Parse Lnx response body
-	var lnxResp LingvanexResponseBody
-	err := json.Unmarshal(body, &lnxResp)
+	body, err := json.Marshal(requestBody)
 	if err != nil {
 		return nil, err
 	}
 
-	return json.Marshal(lnxResp.TranslatedText)
+	return body, err
+}
+
+func ToResourceResponseBody(body []byte) ([]byte, error) {
+	var resBody TranslationServiceResponseBody
+	err := json.Unmarshal(body, &resBody)
+	if err != nil {
+		return nil, err
+	}
+
+	return json.Marshal(resBody.TranslatedText)
 }
