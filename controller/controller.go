0cafe707c89121e77af72328cb54c632b6be819d
Moritz Haller <mhaller@brave.com>
2022-07-01 13:43:19 +0200

    wip

--- controller/controller.go
+++ controller/controller.go
@@ -1,12 +1,15 @@
 package controller
 
 import (
+	"bytes"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 	"os"
 	"path/filepath"
+	"strconv"
 	"time"
 
 	"github.com/brave/go-translate/language"
@@ -15,26 +18,159 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-// LnxEndpoint specifies the remote Lnx translate server used by
-// brave-core, and it can be set to a mock server during testing.
-var LnxEndpoint = os.Getenv("LNX_HOST")
-var LnxApiKey = os.Getenv("LNX_API_KEY")
-var languagePath = "/get-languages"
-var translatePath = "/translate"
+var TranslationServiceHost = os.Getenv("LNX_HOST")
+var TranslationServiceApiKey = os.Getenv("LNX_API_KEY")
 
-// TranslateRouter add routers for translate requests and translate script
-// requests.
-func TranslateRouter() chi.Router {
+func Router() chi.Router {
 	r := chi.NewRouter()
 
-	r.Post("/translate_a/t", Translate)
-	r.Get("/translate_a/l", GetLanguageList)
+	// Relay
+	r.Get("/translate_a/l", RelayLanguageListRequest)
+	r.Post("/translate_a/t", RelayTranslateRequest)
 
+	// Static
 	r.Get("/static/v1/element.js", ServeStaticFile)
 	r.Get("/static/v1/js/element/main.js", ServeStaticFile)
 	r.Get("/static/v1/css/translateelement.css", ServeStaticFile)
 
 	return r
+}
+
+func RelayLanguageListRequest(w http.ResponseWriter, _ *http.Request) {
+	// Relay request to target
+	res, err := RequestLanguageListFromTarget()
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error requesting language list from target: %v", err), http.StatusInternalServerError)
+	}
+
+	if res.StatusCode != http.StatusOK { // TODO: needed? Copy resonse body if status is not OK)
+		_, err = io.Copy(w, res.Body)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Error copying response body: %v", err), http.StatusInternalServerError)
+		}
+		return
+	}
+
+	resBody, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error reading response body: %v", err), http.StatusInternalServerError)
+		return
+	}
+
+	// Convert response
+	convertedResBody, err := language.ToSourceResponseBody(resBody)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error converting to response body: %v", err), http.StatusInternalServerError)
+		return
+	}
+
+	// Respond to source
+	w.Header().Set("Content-Type", res.Header["Content-Type"][0])
+	w.WriteHeader(res.StatusCode)
+
+	_, err = w.Write(convertedResBody)
+	if err != nil {
+		log.Errorf("Error writing response body: %v", err)
+	}
+}
+
+func RequestLanguageListFromTarget() (*http.Response, error) {
+	req, err := http.NewRequest("GET", TranslationServiceHost+"/get-languages", nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Add("Authorization", "Bearer "+TranslationServiceApiKey)
+
+	client := getHTTPClient()
+	res, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer func() {
+		err := res.Body.Close()
+		if err != nil {
+			log.Errorf("Error closing response body stream: %v", err)
+		}
+	}()
+
+	return res, err
+}
+
+func RelayTranslateRequest(w http.ResponseWriter, r *http.Request) {
+	// Build request body for target
+	body, err := translate.BuildTargetRequestBody(r)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error building target request body: %v", err), http.StatusBadRequest)
+	}
+
+	// Relay request to target
+	res, err := RequestTranslationFromTarget(body)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error requesting translation from target: %v", err), http.StatusInternalServerError)
+	}
+
+	if res.StatusCode != http.StatusOK { // TODO: needed? Copy resonse body if status is not OK)
+		w.WriteHeader(res.StatusCode)
+		_, err = io.Copy(w, res.Body)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Error copying target response body: %v", err), http.StatusInternalServerError)
+		}
+		return
+	}
+
+	resBody, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error reading target response body: %v", err), http.StatusInternalServerError)
+		return
+	}
+
+	convertedBody, err := translate.ToResourceResponseBody(resBody)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error converting to response body: %v", err), http.StatusInternalServerError)
+		return
+	}
+
+	// Respond to source
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Content-Type", res.Header["Content-Type"][0])
+	w.WriteHeader(res.StatusCode)
+
+	_, err = w.Write(convertedBody)
+	if err != nil {
+		log.Errorf("Error writing body: %v", err)
+	}
+}
+
+func RequestTranslationFromTarget(body []byte) (*http.Response, error) {
+	targetUrl := TranslationServiceHost + "/translate"
+	u, err := url.Parse(targetUrl)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest("POST", u.String(), bytes.NewBuffer(body))
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Content-Length", strconv.FormatInt(req.ContentLength, 10))
+	req.Header.Add("Authorization", "Bearer "+TranslationServiceApiKey)
+
+	client := getHTTPClient()
+	res, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer func() {
+		err := res.Body.Close()
+		if err != nil {
+			log.Errorf("Error closing target response body stream: %v", err)
+		}
+	}()
+
+	return res, err
 }
 
 func ServeStaticFile(w http.ResponseWriter, r *http.Request) {
@@ -55,120 +191,5 @@ func ServeStaticFile(w http.ResponseWriter, r *http.Request) {
 func getHTTPClient() *http.Client {
 	return &http.Client{
 		Timeout: time.Second * 10,
-	}
-}
-
-// GetLanguageList send a request to Lingvanex server and convert the response
-// into google format and reply back to the client.
-func GetLanguageList(w http.ResponseWriter, r *http.Request) {
-	// Send a get language list request to Lnx
-	req, err := http.NewRequest("GET", LnxEndpoint+languagePath, nil)
-	req.Header.Add("Authorization", "Bearer "+LnxApiKey)
-
-	if err != nil {
-		http.Error(w, fmt.Sprintf("Error creating Lnx request: %v", err), http.StatusInternalServerError)
-		return
-	}
-
-	client := getHTTPClient()
-	lnxResp, err := client.Do(req)
-	if err != nil {
-		http.Error(w, fmt.Sprintf("Error sending request to Lnx server: %v", err), http.StatusInternalServerError)
-		return
-	}
-	defer func() {
-		err := lnxResp.Body.Close()
-		if err != nil {
-			log.Errorf("Error closing response body stream: %v", err)
-		}
-	}()
-
-	// Set response header
-	w.Header().Set("Content-Type", lnxResp.Header["Content-Type"][0])
-	w.WriteHeader(lnxResp.StatusCode)
-
-	// Copy resonse body if status is not OK
-	if lnxResp.StatusCode != http.StatusOK {
-		_, err = io.Copy(w, lnxResp.Body)
-		if err != nil {
-			http.Error(w, fmt.Sprintf("Error copying Lnx response body: %v", err), http.StatusInternalServerError)
-		}
-		return
-	}
-
-	// Convert to google format language list and write it back
-	lnxBody, err := ioutil.ReadAll(lnxResp.Body)
-	if err != nil {
-		http.Error(w, fmt.Sprintf("Error reading Lnx response body: %v", err), http.StatusInternalServerError)
-		return
-	}
-	body, err := language.ToGoogleLanguageList(lnxBody)
-	if err != nil {
-		http.Error(w, fmt.Sprintf("Error converting to google language list: %v", err), http.StatusInternalServerError)
-		return
-	}
-	_, err = w.Write(body)
-	if err != nil {
-		log.Errorf("Error writing response body for translate requests: %v", err)
-	}
-}
-
-// Translate converts a Google format translate request into a Lingvanex format
-// one which will be send to the Lingvanex server, and write a Google format
-// response back to the client.
-func Translate(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Access-Control-Allow-Origin", "*") // same as Google response
-
-	req, isAuto, err := translate.ToLingvanexRequest(r, LnxEndpoint+translatePath)
-	if err != nil {
-		http.Error(w, fmt.Sprintf("Error converting to LnxEndpoint request: %v", err), http.StatusBadRequest)
-		return
-	}
-
-	req.Header.Add("Authorization", "Bearer "+LnxApiKey)
-
-	// Send translate request to Lnx server
-	client := getHTTPClient()
-	lnxResp, err := client.Do(req)
-	if err != nil {
-		http.Error(w, fmt.Sprintf("Error sending request to LnxEndpoint: %v", err), http.StatusInternalServerError)
-		return
-	}
-	defer func() {
-		err := lnxResp.Body.Close()
-		if err != nil {
-			log.Errorf("Error closing response body stream: %v", err)
-		}
-	}()
-
-	// Set Header
-	w.Header().Set("Content-Type", lnxResp.Header["Content-Type"][0])
-	w.Header().Set("Access-Control-Allow-Origin", "*") // same as Google response
-
-	// Copy resonse body if status is not OK
-	if lnxResp.StatusCode != http.StatusOK {
-		w.WriteHeader(lnxResp.StatusCode)
-		_, err = io.Copy(w, lnxResp.Body)
-		if err != nil {
-			http.Error(w, fmt.Sprintf("Error copying LnxEndpoint response body: %v", err), http.StatusInternalServerError)
-		}
-		return
-	}
-
-	// Set google format response body
-	lnxBody, err := ioutil.ReadAll(lnxResp.Body)
-	if err != nil {
-		http.Error(w, fmt.Sprintf("Error reading LnxEndpoint response body: %v", err), http.StatusInternalServerError)
-		return
-	}
-	body, err := translate.ToGoogleResponseBody(lnxBody, isAuto)
-	if err != nil {
-		http.Error(w, fmt.Sprintf("Error converting to google response body: %v", err), http.StatusInternalServerError)
-		return
-	}
-	w.WriteHeader(lnxResp.StatusCode)
-	_, err = w.Write(body)
-	if err != nil {
-		log.Errorf("Error writing response body for translate requests: %v", err)
 	}
 }
